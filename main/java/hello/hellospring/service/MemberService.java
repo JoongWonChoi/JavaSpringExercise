0a06cc7971d6ab995a6b6e2b231cb580abbf5702
JoongWonChoi <qweasd1130@naver.com>
2021-12-30 15:48:09 +0900

    Add MemberServiceTest & Add View Function

--- main/java/hello/hellospring/service/MemberService.java
+++ main/java/hello/hellospring/service/MemberService.java
@@ -3,21 +3,28 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
     // cntr+shift+t ===> Test 페이지 제작
 
-    private final MemberRepository memberrepo = new MemoryMemberRepository(); //MemberRepository는 인터페이스이기때문에 구현체는 반드시 구현 클래스 인스턴스여야함.
+    private final MemberRepository memberRepo; //memberRepository는 인터페이스이기때문에 구현체는 반드시 구현 클래스 인스턴스여야함.
+
+    public MemberService(MemberRepository memberRepo){
+        this.memberRepo = memberRepo;
+    }
 
     // 회원가입
     public Long join(Member member){
         //같은 이름의 중복 회원 X
         validateDuplicateName(member); // (cntl+alt+shift+t)
         //
-        memberrepo.save(member);
+        memberRepo.save(member);
         return member.getId();
 
 
@@ -25,20 +32,20 @@ public class MemberService {
     }
     // 같은 이름의 중복 회원 판별 로직
     private void validateDuplicateName(Member member){
-        memberrepo.findByName(member.getName()) //Optional<member> 반환이기 때문에 바로 ifPresent메소드 연결 가능
+        memberRepo.findByName(member.getName()) //Optional<member> 반환이기 때문에 바로 ifPresent메소드 연결 가능
                 .ifPresent( m -> {
-                    throw new IllegarStateException("already exists Name . .");
+                    throw new IllegalStateException("already exists Name . .");
                 });
     }
 
     // 회원 검색
     public Optional<Member> findOneMember(Long memberId){ //why optional??
-        return memberrepo.findById(memberId);
+        return memberRepo.findById(memberId);
     }
 
     //전체 회원 조회
     public List<Member> findMembers(){
-        return memberrepo.findAll();
+        return memberRepo.findAll();
     }
 
 }
--- test/java/hello/hellospring/service/MemberServiceTest.java
+++ test/java/hello/hellospring/service/MemberServiceTest.java
@@ -2,25 +2,66 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
     MemoryMemberRepository mmr = new MemoryMemberRepository();
+    MemberService memberService = new MemberService();
 
+
+    //shift+f10 ==> 이전 run restart
+    @AfterEach
+    public void clearRepo(){
+        mmr.clearStore();
+    }
 
     @Test
     void 회원등록() {
-        //given ==> 회원등록
+        //given (테스트를 진행할 검증 데이터) ==> member의 이름 등록
         Member member = new Member();
         member.setName("hello");
-        mmr.save(member);
+        //when (특정 Event처리 테스트) ==> 회원가입
+        Long saveId = memberService.join(member); //회원가입 후 반환되는 id를 saveId 변수에 저장
+        //then (테스트 결과)
+        Member findName = memberService.findOneMember(saveId).get();// ==> saveId id를 통해 회원을 찾아 이름에 접근하고 Optional의 get() 메서드로 값 반환.
+                                                                    //findOneMember는 MemoryMemberRepository의 findById 메서드를 호출 . . 이 메서드는 Optional type으로 wrapping된 Member자료형을 반환하기 때문에 findName 변수의 자료형은 Member.
 
-        //when ==>
+        assertThat(findName.getName()).isEqualTo(member.getName());
 
 
-        //then
+    }
+
+    @Test
+    void 회원_중복_테스트(){ //설정한 validateDuplicateName 메서드가 정상적으로 작동되는지 판별하는 테스트 로직
+        //given ==> 회원 등록
+        Member member1 = new Member();
+        member1.setName("hello");
+
+        Member member2 = new Member();
+        member2.setName("hello");
+        //when
+        memberService.join(member1);
+
+        //  ===== try-catch절로 오류 검증 테스트 =====
+        /*try{
+            memberService.join(member2);
+            fail("Error must be existed . .");
+        }
+        catch(IllegalStateException e){ //catch구문까지 오면 예외가 정상적으로 작동 된 것.
+        }
+        */ // ===> 단체 주석 : cntr + shift + /
+
+        IllegalStateException e = assertThrows(IllegalStateException.class,  // cntr+alt+v -> 자동 자료형 + 변수명 생성
+                () -> memberService.join(member2));//예외가 발생해야 함
+//then
+        assertThat(e.getMessage()).isEqualTo("already exists Name . .");
+
 
 
     }
